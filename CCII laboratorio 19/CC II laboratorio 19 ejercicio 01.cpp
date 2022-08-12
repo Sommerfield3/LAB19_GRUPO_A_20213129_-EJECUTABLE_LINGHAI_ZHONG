6754ca2302a1a1447ed6e5bc120280a6db75fc88
Linghai Joaquin, Zhong Callasi <104087488+Sommerfield3@users.noreply.github.com>
2022-08-11 23:48:04 -0500

    Add files via upload

---
+++ CCII laboratorio 19/CC II laboratorio 19 ejercicio 01.cpp
@@ -0,0 +1,82 @@
+/*1. Utilizando Functores, elabore una clase que calcule la ecuación de regresión lineal
+simple (y = a + bc) de un conjunto de pares de datos (x, y) almacenados en un
+vector, retorne como parte del resultado los valores de a y b. Apóyese en functores
+para calcular las diferentes sumatorias que pueden presentarse.*/
+/*Apellidos y Nombres: Zhong Callasi, Linghai Joaquin*/
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+class RegresionLinealSimple{
+private:
+	vector<float> x;
+	vector<float> y;
+	vector<float> xy;
+	vector<float> xpow2;
+	float sumatoriaX;
+	float sumatoriaY;
+	float sumatoriaXY;
+	float sumatoriaXpow2;
+public:
+	float operator()(vector<float> a){
+		float sumatoria=0;
+		int tam1=a.size();
+		for (int i=0;i<tam1;i++){
+			sumatoria+=a[i];
+		}
+		return sumatoria;
+	}
+	void valores(vector<vector<float>> paresOrdenados){
+		x.clear();
+		y.clear();
+		xy.clear();
+		xpow2.clear();
+		int tamVector=paresOrdenados.size();
+		for (int i=0;i<tamVector;i++){
+			x.push_back(paresOrdenados[i][0]);
+			y.push_back(paresOrdenados[i][1]);
+			xy.push_back(x[i]*y[i]);
+			xpow2.push_back(x[i]*x[i]);
+		}
+		RegresionLinealSimple operac;/*Usando functores para las sumatorias.*/
+		sumatoriaX=operac(x);
+		sumatoriaY=operac(y);
+		sumatoriaXY=operac(xy);
+		sumatoriaXpow2=operac(xpow2);
+	}
+	tuple<float,float> regresionSimple(vector<vector<float>> paresOrdenados){
+		int n=paresOrdenados.size();
+		this->valores(paresOrdenados);
+		float a;
+		float b;
+		b=((n*sumatoriaXY)-(sumatoriaX*sumatoriaY))/((n*sumatoriaXpow2)-((sumatoriaX)*(sumatoriaX)));
+		a=(sumatoriaY/n)-(b*(sumatoriaX/n));
+		return make_tuple(a,b);
+	}
+};
+int main(int argc, char *argv[]) {
+	tuple<float,float> resultado;
+	RegresionLinealSimple Regresion;
+	vector<vector<float>> paresOrdenados{{1,2},{2,3},{2,4},{3,4},{4,4},{4,6},{5,5},{6,7}};
+	resultado=Regresion.regresionSimple(paresOrdenados);
+	cout<<"Para la lista de pares ordenados: {";
+	int tam=paresOrdenados.size();
+	for (int i=0;i<tam;i++){
+		if (tam==1){
+			cout<<paresOrdenados[i][0]<<", "<<paresOrdenados[i][1];
+		}
+		else if (i==(tam-1)){
+			cout<<"{"<<paresOrdenados[i][0]<<", "<<paresOrdenados[i][1]<<"}";
+		}
+		else{
+			cout<<"{"<<paresOrdenados[i][0]<<", "<<paresOrdenados[i][1]<<"}, ";
+		}
+	}
+	cout<<"}"<<endl;
+	cout<<"Valores de su ecuación de regresión lineal simple (y=a+bx): "<<endl<<endl;
+	cout<<"Valor de 'a': "<<get<0>(resultado)<<endl;
+	cout<<"Valor de 'b': "<<get<1>(resultado);
+	return 0;
+}
+
---
+++ CCII laboratorio 19/CC II laboratorio 19 ejercicio 03.cpp
@@ -0,0 +1,92 @@
+/*3. Utilizando Functores y el método std::sort, elabore una clase Elementos con dos
+atributos de números enteros a y b. Genere una lista de 20 elementos de esta clase
+Elementos con valores aleatorios tanto para a y b. Mediante el método std::sort
+ordénelos de la forma en que un Objeto al ser comparado con un segundo se tenga
+la desigualdad : obj1.a < obj2.b . El método std::sort debe de trabajar con un
+objeto Functores. De ser necesario, investigue como realizar este procedimiento
+que dependa de un objeto del tipo Functores.*/
+/*Apellidos y Nombres: Zhong Callasi, Linghai Joaquin*/
+#include <iostream>
+#include<cstdlib>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Elementos{
+public:
+	int a;
+	int b;
+	void operator()(Elementos &m, Elementos &n){
+		int numero1=m.a;
+		int numero2=n.b;
+		vector<int> menorAmayorB;
+		menorAmayorB.push_back(numero1);
+		menorAmayorB.push_back(numero2);
+		sort(menorAmayorB.begin(),menorAmayorB.end());
+		m.asignarAtributos(menorAmayorB[0],m.b);
+		n.asignarAtributos(n.a,menorAmayorB[1]);
+	}
+	void asignarAtributos(int x, int y){
+		a=x;
+		b=y;
+	}
+	void imprimirAtributos(){
+		cout<<"{"<<a<<", "<<b<<"}";
+	}
+};
+int main(int argc, char *argv[]) {
+	srand(time(0));
+	vector<Elementos> ElementosLista(20);
+	int t=ElementosLista.size();
+	for (int j=0;j<t;j++){
+		int val1=rand()%1000; /*Se le coloca "%1000" para darle un rango de números de entre 0 a 999 solo por darle un rango.*/
+		int val2=rand()%1000;
+		ElementosLista[j].asignarAtributos(val1,val2);
+	}
+	cout<<"Lista inicial: "<<endl;
+	for (int i=0;i<t;i++){
+		if (t==1){
+			ElementosLista[i].imprimirAtributos();
+		}
+		else if (i==0){
+			cout<<"{";
+			ElementosLista[i].imprimirAtributos();
+		}
+		else if (i==(t-1)){
+			ElementosLista[i].imprimirAtributos();
+			cout<<"}";
+		}
+		else{
+			ElementosLista[i].imprimirAtributos();
+			cout<<", ";
+		}
+	}
+	int total=(t-1);
+	while (total>0){
+		Elementos functAplic;
+		for (int s=0;s<total;s++){
+			ElementosLista[s](ElementosLista[s],ElementosLista[s+1]);
+		}
+		total--;
+	}
+	cout<<endl<<"Lista con el orden requerido: "<<endl;
+	for (int i=0;i<t;i++){
+		if (t==1){
+			ElementosLista[i].imprimirAtributos();
+		}
+		else if (i==0){
+			cout<<"{";
+			ElementosLista[i].imprimirAtributos();
+		}
+		else if (i==(t-1)){
+			ElementosLista[i].imprimirAtributos();
+			cout<<"}";
+		}
+		else{
+			ElementosLista[i].imprimirAtributos();
+			cout<<", ";
+		}
+	}
+	return 0;
+}
+
